29dbb5657b27fa094e126c9c872b6a14b608bfb9
Aidas <aidas@bitlgx.com>
2020-05-25 12:35:18 +0300

    inactive client state now works too:

--- app/Http/Controllers/ClientController.php
+++ app/Http/Controllers/ClientController.php
@@ -8,12 +8,32 @@ use Illuminate\Http\Request;
 
 class ClientController extends Controller
 {
+    /**
+     * @return JsonResponse
+     */
     public function connect(): JsonResponse
     {
 
         // Get the user and change its client state
         $user = auth()->user();
         $user->client_connected = true;
+        $user->save();
+
+        // Emit the event
+        event(new ClientStateChanged($user->client_connected));
+
+        return response()->json([
+            'status' => 'ok'
+        ]);
+    }
+
+    /**
+     * @return JsonResponse
+     */
+    public function disconnect(): JsonResponse {
+        // Get the user and change its client state
+        $user = auth()->user();
+        $user->client_connected = false;
         $user->save();
 
         // Emit the event
--- resources/views/livewire/board/buttons.blade.php
+++ resources/views/livewire/board/buttons.blade.php
@@ -10,7 +10,11 @@
         @if($votingAllowed)
             <button type="button" class="btn btn-danger btn-block" wire:click="stopVoting()">Stop Voting!</button>
         @else
-            <button type="button" class="btn btn-primary btn-block" wire:click="allowVoting()">Allow Voting!</button>
+            <button type="button" class="btn btn-primary btn-block" wire:click="allowVoting()"
+
+                    @if(!$clientStatus) disabled @endif
+            >Allow Voting!
+            </button>
         @endif
     </div>
 
